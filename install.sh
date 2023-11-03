67f1d632a328dffec51db1589884dceeb43faa3d
omidima <47784584+omidima@users.noreply.github.com>
2023-11-03 13:58:05 +0000

    Updata install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 pip install pyyml
-sudo cp omidocker.py /var/omidocker.py
+sudo apt-get install curl -y
 
-echo 'alias omidocker="python /var/omidocker.py"' >> ~/.zshrc
-source ~/.zshrc
+curl https://raw.githubusercontent.com/omidima/omidocker/main/omidocker.py > omidocker.py
+sudo mv omidocker.py /var/omidocker.py
 
-echo 'alias omidocker="python /var/omidocker.py"' >> ~/.bashrc
-source ~/.bashrc
+echo "alias omidocker='python /var/omidocker.py'" >> ~/.bashrc 
+echo "alias omidocker='python /var/omidocker.py'" >> ~/.zshrc 
+echo "alias omidocker='python /var/omidocker.py'" >> ~/.bash_profile 
+
+source ~/.bashrc 
+source ~/.zshrc 
+source ~/.bash_profile 
