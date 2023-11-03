6a55d597779a42e87d9122824786eedadc53c27a
omidima <47784584+omidima@users.noreply.github.com>
2023-11-03 15:01:49 +0000

    Fix error: installing python3

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+if ! command -v python3 &> /dev/null; then
+    echo "Python not found. Installing Python3 and pip..."
+    sudo apt-get update
+    sudo apt-get install python3 -y
+    sudo apt-get install python3-pip -y
+fi
 
-pip install pyyml
+pip3 install pyyml
 sudo apt-get install curl -y
 
 curl https://raw.githubusercontent.com/omidima/omidocker/main/omidocker.py > omidocker.py
 sudo mv omidocker.py /var/omidocker.py
 
-echo "alias omidocker='python /var/omidocker.py'" >> ~/.bashrc 
-echo "alias omidocker='python /var/omidocker.py'" >> ~/.zshrc 
-echo "alias omidocker='python /var/omidocker.py'" >> ~/.bash_profile 
+echo "alias omidocker='python3 /var/omidocker.py'" >> ~/.bashrc 
+echo "alias omidocker='python3 /var/omidocker.py'" >> ~/.zshrc 
+echo "alias omidocker='python3 /var/omidocker.py'" >> ~/.bash_profile 
 
 source ~/.bashrc 
 source ~/.zshrc 
